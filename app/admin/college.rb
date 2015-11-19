d75b440c47e4599f08e141896223dee508054a7d
miannelli <miiannelli@gmail.com>
2015-11-19 14:36:27 -0500

    Fixed an issue where country selection was breaking entire colleges form.

--- app/admin/college.rb
+++ app/admin/college.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register College do
   menu :label => "Colleges and Affiliations"
+
+
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :street
+      f.input :city
+      f.input :state
+      f.input :country
+    end
+
+
+    f.actions
+  end
+
+
+
 end
